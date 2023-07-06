015aa7cc06b18181f51126807d33b780e82777e8
Dominik Pustofka <dominik.pustofka@gmx.net>
2023-07-06 19:32:19 +0200

    mustache hinzugefügt

---
+++ src/main/resources/static/render.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var templates = document.getElementsByClassName("dynamic-content");
+    var data = {
+        title: "Eingabeformular zur Auswahl kombinierter Ressourcen",
+        headerLeft: "Technische Rahmenbedingungen am Aufstellort",
+        left0: "Durschnittliche Raumtemperatur:",
+        left1: "Relative Luftfeuchtigkeit:",
+        left2: "Max. Verfügbare Pneumatikversorgung:",
+        left3: "Max. Verfügbare Stromversorgung:",
+        elect0:"Wechselstrom (230 V, 16 A)",
+        elect1:"Drehstrom (400 V, 16 A)",
+        elect2:"Drehstrom (400 V, 32 A)",
+        elect3:"Drehstrom (400 V, 63 A",
+        headerRight:"Wirtschaftliche Rahmenbedingungen am Aufstellort",
+        right0: "Flächenkosten (pro m²):",
+        right1: "Geplante Einsatzdauer in Monaten:",
+        right2: "Vorhandene Ressourcen:",
+        start: "Anfrage starten",
+        assurances: "Anforderungsdatei (XML/JSON)"
+
+
+    };
+
+    for (var i = 0; i < templates.length; i++) {
+        var template = templates[i].innerHTML;
+        var renderedHTML = Mustache.render(template, data);
+        templates[i].innerHTML = renderedHTML;
+    }
+});
